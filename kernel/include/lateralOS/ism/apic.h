12ff58f4e61a754105d193f4369634b6af7e055f
teslaNova <terranova@fahr-zur-hoelle.org>
2012-04-27 20:12:01 +0200

    implemented io apic. ints redirected and bsp lapic initiated, just need do adjust it a bit later on for sipis

--- kernel/include/lateralOS/ism/apic.h
+++ kernel/include/lateralOS/ism/apic.h
@@ -20,6 +20,57 @@
 		http://f.osdev.org/viewtopic.php?f=1&t=23582
 */
 
+enum {
+	LAPIC_REG_ID 	= 0x20,
+	LAPIC_REG_VER	= 0x30,
+	LAPIC_REG_TPR	= 0x80,
+	LAPIC_REG_APR	= 0x90,
+	LAPIC_REG_PPR	= 0xA0,
+	LAPIC_REG_EOI	= 0xB0,
+	LAPIC_REG_RRD	= 0xC0,
+	LAPIC_REG_LDR	= 0xD0,
+	LAPIC_REG_DFR	= 0xE0,
+	LAPIC_REG_SPV	= 0xF0,
+
+	// ..
+
+	LAPIC_REG_ERR	= 0x280,
+
+	// ..
+
+	LAPIC_REG_ISR 	= 0x100,
+	LAPIC_REG_TMR	= 0x180,
+	LAPIC_REG_IRR	= 0x200,
+
+	// ...
+
+	LAPIC_REG_LINT0	= 0x350,
+	LAPIC_REG_LINT1 = 0x360,
+	LAPIC_REG_LERR	= 0x370,
+
+	// ..
+};
+
+enum {
+	LAPIC_SPV_ENA	= (1 << 8),
+	LAPIC_DFR_FLAT 	= (0xF << 28),
+
+
+	LAPIC_LVT_DM_EXT= (0b111 << 8),
+	LAPIC_LVT_DM_NMI= (1 << 10),
+
+	LAPIC_LVT_TM_LVL= (1 << 15),
+
+	LAPIC_LVT_POL_LO= (1 << 13),
+
+	LAPIC_LVT_MASK	= (1 << 16),
+};
+
+enum {
+	LAPIC_MSR_ENA	= (1 << 11),
+	LAPIC_MSR_BSP	= (1 << 8),
+};
+
 struct io_apic {
 	uint base_addr;
 };
@@ -37,7 +88,35 @@ void
 apic_eoi(uint int_no);
 
 void
+apic_local_write(uint reg, uint data);
+
+uint
+apic_local_read(uint reg);
+
+void
 apic_io_set(uint base_addr);
+
+void
+apic_io_write(uint n, uint reg, uint data);
+
+uint
+apic_io_read(uint n, uint reg);
+
+enum {
+	IOAPIC_MREG_SEL = 0,
+	IOAPIC_MREG_WIN = 0x10,
+
+	IOAPIC_REG_ID	= 0,
+	IOAPIC_REG_VER,
+	IOAPIC_REG_ARB,
+
+	IOAPIC_REG_RDT	= 0x10,
+
+	IOAPIC_RDT_NUM	= 24,
+
+	IOAPIC_DM_EXT 	= (0b111 << 8),
+	IOAPIC_DM_NMI	= (0b100 << 8),
+};
 
 void
 apic_map(void);
--- kernel/ism/apic.c
+++ kernel/ism/apic.c
@@ -9,16 +9,42 @@
 
 #include <lateralOS/mm/vmm.h>
 
+#include <lateralOS/cpu.h>
+
 struct io_apic io_apic_tbl[IO_APIC_NUM] = {0};
 
 void
 apic_enable(void) {
+	struct cpu *cpu = cpu_get_local();
+	uint i=0, j;
 
+	cpu_msr_write(MSR_APIC_BASE, cpu->lapic_base 
+			| LAPIC_MSR_ENA
+			| (cpu->is_bsp ? LAPIC_MSR_BSP : 0));
+
+	apic_local_write(LAPIC_REG_SPV, LAPIC_SPV_ENA | 0xFF);
+	apic_local_write(LAPIC_REG_DFR, LAPIC_DFR_FLAT);
+
+	apic_local_write(LAPIC_REG_TPR, 0);
+
+	apic_local_write(LAPIC_REG_LINT0, 0x20 | LAPIC_LVT_DM_EXT);
+	apic_local_write(LAPIC_REG_LINT1, 0x40 | LAPIC_LVT_DM_NMI);
+
+	k_printf("\t\t- err: %x\r\n", 0xFF & apic_local_read(LAPIC_REG_LERR));
+	
+	for(j=0; j<IO_APIC_NUM; j++) {
+		for(i=0; i<IOAPIC_RDT_NUM; i++) {
+			apic_io_write(j, IOAPIC_REG_RDT + (i * 2), ((0x20 + i) + (j * IOAPIC_RDT_NUM)) | IOAPIC_DM_EXT);
+		}
+	}
 }
 
 void
 apic_disable(void) {
+	cpu_msr_write(MSR_APIC_BASE, cpu_get_local()->lapic_base 
+			| (cpu_get_local()->is_bsp ? LAPIC_MSR_BSP : 0));
 
+	apic_local_write(LAPIC_REG_SPV, 0);
 }
 
 void
@@ -28,7 +54,17 @@ apic_init(void) {
 
 void
 apic_eoi(uint int_no) {
+	apic_local_write(LAPIC_REG_EOI, 0);
+}
 
+void
+apic_local_write(uint reg, uint data) {
+	*(uint *)(cpu_get_local()->lapic_base + reg) = data;
+}
+
+uint
+apic_local_read(uint reg) {
+	return *(uint *)(cpu_get_local()->lapic_base + reg);
 }
 
 void
@@ -36,16 +72,60 @@ apic_io_set(uint base_addr) {
 	uint i;
 
 	for(i=0; i<IO_APIC_NUM; i++) {
+		if(io_apic_tbl[i].base_addr == base_addr) {
+			break;
+		}
+
 		if(io_apic_tbl[i].base_addr == 0) {
 			io_apic_tbl[i].base_addr = base_addr;
 			break;
 		}
-	}
-
-	k_printf("0x%x\r\n", base_addr);	
+	}	
 }
 
 void
+apic_io_write(uint n, uint reg, uint data) {
+	uint ioapic = io_apic_tbl[n].base_addr;
+
+	if(ioapic == 0) {
+		return;
+	}
+
+	*(uint *)(ioapic + IOAPIC_MREG_SEL) = reg;
+	*(uint *)(ioapic + IOAPIC_MREG_WIN) = data;
+}
+
+uint
+apic_io_read(uint n, uint reg) {
+	uint ioapic = io_apic_tbl[n].base_addr;
+
+	if(ioapic == 0) {
+		return;
+	}
+
+	*(uint *)(ioapic + IOAPIC_MREG_SEL) = reg;
+	return *(uint *)(ioapic + IOAPIC_MREG_WIN);
+}
+
+/* NOT FINISHED YET */
+void
 apic_map(void) {
-	
+	uint i;
+	struct vmm_flags flags = {.present = 1, .writeable = 1, .privileged = 1};
+
+	/* map each lapic and ioapic base addr */
+	for(i=0; i<IO_APIC_NUM; i++) {
+		if(io_apic_tbl[i].base_addr == 0) {
+			//*(byte *)(io_apic_tbl[i].base_addr) = '0';
+			break;
+		}
+
+		vmm_map(flags, io_apic_tbl[i].base_addr, io_apic_tbl[i].base_addr);
+	}
+
+	for(i=0; i<cpu_count(); i++) {
+		uint lapic_base = cpu_get(i)->lapic_base;
+
+		vmm_map(flags, lapic_base, lapic_base);
+	}
 }
